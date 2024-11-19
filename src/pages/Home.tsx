c0b9311fa222091e511e0d577a93b69ac258b202
Brandon Palencia <22011436@galileo.edu>
2024-11-18 20:37:40 -0600

    Entrega de la aplicación híbrida perteneciente a la entrega número 3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,27 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React from 'react';
+import { useState } from 'react';
+import { IonInput, IonItem, IonList } from '@ionic/react';
+
+import { IonButton } from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
+  let [monto, setMonto] = useState<number | "">("")
+  let [montoFinal, setMontoFinal] = useState<number | ''>('');
+
+  function convertir(){
+    setMontoFinal(monto * 8);
+  };
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+    <>
+      <div className='container'>
+        <p>Ingrese el monto a convertir en $$$</p>
+        <input type='number' value={monto} onChange={(e) => setMonto(Number(e.target.value))}></input>
+        <IonButton onClick={convertir}>Convertir</IonButton>
+        <input type='number' value={montoFinal}></input>
+      </div>
+    </>
   );
 };
 
