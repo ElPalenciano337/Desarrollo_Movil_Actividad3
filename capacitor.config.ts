83e146bd273c8b2ee1675ea2edde3b9a7aca12ad
Brandon Palencia <22011436@galileo.edu>
2024-11-18 19:53:22 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Conversor de Divisas',
+  webDir: 'dist'
+};
+
+export default config;
